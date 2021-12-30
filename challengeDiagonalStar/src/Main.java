c087c1f379122a6a708827081192c82781cc4d00
Trojan0101 <aloysius.mon.05@gmail.com>
2021-12-30 11:38:49 +0530

    Diagonal star problem

--- challengeDiagonalStar/src/Main.java
+++ challengeDiagonalStar/src/Main.java
@@ -1,11 +1,25 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    printSquareStar(5);
     }
 
     public static void printSquareStar(int number) {
 
-    }
+        if (number < 5) {
+            System.out.println("Invalid value");
+        }
 
+        for (int row = 1; row <= number; row++) {
+            for (int col = 1; col <= number; col++) {
+                if (row == 1 || row == number || col == 1 || col == number || row == col || (col == (number -  row) + 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
+

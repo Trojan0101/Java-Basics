4b92098c5da10e373be96ebf5999ee473d1618ed
Trojan0101 <aloysius.mon.05@gmail.com>
2021-12-30 12:43:34 +0530

    Reading user input

---
+++ ReadingUserInput/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	    Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your birth year: ");
+
+        boolean hasNextInt = scanner.hasNextInt();
+
+        if (hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); // In-order to handle next line character (enter key)
+            int age = 2021 - yearOfBirth;
+
+            System.out.println("Enter you name: ");
+            String name = scanner.nextLine();
+
+            if (age >= 0 && age <= 100) {
+                System.out.println("Your name is " + name + " and you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid year of birth");
+            }
+        } else {
+            System.out.println("Unable to parse year of birth.");
+        }
+        scanner.close();
+
+    }
+}

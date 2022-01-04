c1dba7442fa91819cd2783c8efde8602f784a0da
Trojan0101 <aloysius.mon.05@gmail.com>
2022-01-04 13:17:40 +0530

    Composition challenge

---
+++ challengeComposition/src/Lamp.java
@@ -0,0 +1,28 @@
+public class Lamp {
+
+    private String style;
+    private boolean isBattery;
+    private int globRating;
+
+    public Lamp(String style, boolean isBattery, int globRating) {
+        this.style = style;
+        this.isBattery = isBattery;
+        this.globRating = globRating;
+    }
+
+    public void turnOn() {
+        System.out.println("Lamp -> Turning on");
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public boolean isBattery() {
+        return isBattery;
+    }
+
+    public int getGlobRating() {
+        return globRating;
+    }
+}

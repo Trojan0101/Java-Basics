e4a72ad9d0f0ceae5b4cbf4d31d3e87586613ad0
Trojan0101 <aloysius.mon.05@gmail.com>
2022-01-04 15:34:40 +0530

    Encapsulation challenge

---
+++ challengeEncapsulation/src/Printer.java
@@ -0,0 +1,39 @@
+public class Printer {
+
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean isDuplex;
+
+    public Printer(int tonerLevel, boolean isDuplex) {
+        this.tonerLevel = (tonerLevel > -1 && tonerLevel <= 100) ? tonerLevel:-1;
+        this.pagesPrinted = 0;
+        this.isDuplex = isDuplex;
+    }
+
+    public int addToner(int tonerAmount) {
+        if (tonerAmount > 0 && tonerAmount <= 100) {
+            if (this.tonerLevel + tonerAmount > 100) {
+                return -1;
+            } else {
+                this.tonerLevel += tonerAmount;
+                return this.tonerLevel;
+            }
+        } else {
+            return -1;
+        }
+    }
+
+    public int printPages(int pages) {
+        int pagesToPrint = pages;
+        if (this.isDuplex) {
+            pagesToPrint = (pages / 2) + (pages % 2);
+            System.out.println("Printing in duplex mode");
+        }
+        this.pagesPrinted += pagesToPrint;
+        return pagesToPrint;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+}

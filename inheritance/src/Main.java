6b935ed71565eddc4f32d7e5d7f8b4e0edcfd5f5
Trojan0101 <aloysius.mon.05@gmail.com>
2022-01-04 09:56:30 +0530

    Updated inheritance project

--- inheritance/src/Main.java
+++ inheritance/src/Main.java
@@ -5,7 +5,14 @@ public class Main {
         Animal animal = new Animal("Animal", 1, 1, 5, 5);
         Dog dog = new Dog("German Shepard", 8, 20, 2, 4, 1, 20, "silky");
 
+        animal.eat();
         dog.eat();
+        dog.walk();
+        dog.run();
+
+        Fish fish = new Fish("Shark", 10, 100, 2, 2, 4);
+
+        fish.swim(2);
 
     }
 }

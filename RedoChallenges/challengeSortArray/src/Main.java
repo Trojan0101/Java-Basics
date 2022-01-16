bfe1e27c203372965e51b66c2c9da0f981776a6c
Trojan0101 <aloysius.mon.05@gmail.com>
2022-01-16 11:54:49 +0530

    Re-did array sort

--- RedoChallenges/challengeSortArray/src/Main.java
+++ RedoChallenges/challengeSortArray/src/Main.java
@@ -38,12 +38,13 @@ public class Main {
         for (int i = 0; i < array.length; i++) {
             sortedArray[i] = array[i];
         }
-        int temp;
+
         boolean flag = true;
+        int temp;
         while(flag) {
             flag = false;
             for (int i = 0; i < sortedArray.length - 1; i++) {
-                if (sortedArray[i] < sortedArray[i + 1]) {
+                if (sortedArray[i] < sortedArray[i +  1]) {
                     temp = sortedArray[i];
                     sortedArray[i] = sortedArray[i + 1];
                     sortedArray[i + 1] = temp;

4c752ac731fe6c7295946642adf829b0ac876e33
Trojan0101 <aloysius.mon.05@gmail.com>
2022-01-20 15:02:36 +0530

    Updated: Interface Challenge

--- interfaceChallenge/src/ISaveable.java
+++ interfaceChallenge/src/ISaveable.java
@@ -1,8 +1,9 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public interface ISaveable {
 
     List<String> write();
-    void read(List<String> list);
+    void read(List<String> savedValues);
 
 }
--- interfaceChallenge/src/Player.java
+++ interfaceChallenge/src/Player.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player implements ISaveable{
@@ -47,12 +48,33 @@ public class Player implements ISaveable{
     }
 
     @Override
-    public List<String> write() {
-        return null;
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", weapon='" + weapon + '\'' +
+                ", hitPoints=" + hitPoints +
+                ", strength=" + strength +
+                '}';
     }
 
     @Override
-    public void read(List<String> list) {
+    public List<String> write() {
+        List<String> values = new ArrayList<>();
+        values.add(0, this.name);
+        values.add(1, "" + this.hitPoints);
+        values.add(2, "" + this.strength);
+        values.add(3, this.weapon);
+        return values;
+    }
 
+    @Override
+    public void read(List<String> savedValues) {
+        if (savedValues != null && savedValues.size() > 0) {
+            this.name = savedValues.get(0);
+            this.hitPoints = Integer.parseInt(savedValues.get(1));
+            this.strength = Integer.parseInt(savedValues.get(3));
+            this.weapon = savedValues.get(3);
+
+        }
     }
 }

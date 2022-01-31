a4eee049eb6cb519ec3c4873b68b1d0d50e287a3
Trojan0101 <aloysius.mon.05@gmail.com>
2022-01-31 10:16:49 +0530

    Modified abstract class

--- abstractClass/src/Main.java
+++ abstractClass/src/Main.java
@@ -1,19 +1,33 @@
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Dog dog = new Dog("Yorke");
-        dog.breath();
-        dog.eat();
+//        Dog dog = new Dog("Yorke");
+//        dog.breath();
+//        dog.eat();
+//
+//        Parrot parrot = new Parrot("Australian Parrot");
+//        parrot.eat();
+//        parrot.breath();
+//        parrot.fly();
+//
+//        Penguin penguin = new Penguin("Black");
+//        penguin.fly();
 
-        Parrot parrot = new Parrot("Australian Parrot");
-        parrot.eat();
-        parrot.breath();
-        parrot.fly();
+        System.out.println(twofer(""));
 
-        Penguin penguin = new Penguin("Black");
-        penguin.fly();
 
+
+
+    }
+    public static String twofer(String name) {
+        if (name != null) {
+            return "One for " + name + " , one for me.";
+        } else {
+            return "One for you, one for me.";
+        }
     }
 
 }
